9b020762c3b07b9f70446187a61a72d7b34b7b60
Martin Aspeli <optilude@gmail.com>
2009-06-01 12:38:56 +0000

    Events example
    svn path=/example.conference/trunk/; revision=87235

--- example/conference/presenter.py
+++ example/conference/presenter.py
@@ -4,6 +4,9 @@ from zope import schema
 from plone.directives import form, dexterity
 from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
 
+from zope.app.container.interfaces import IObjectAddedEvent
+from Products.CMFCore.utils import getToolByName
+
 from example.conference import _
 
 from plone.namedfile.field import NamedImage
@@ -30,6 +33,27 @@ class IPresenter(form.Schema):
             title=_(u"Please upload an image"),
             required=False,
         )
+
+@grok.subscribe(IPresenter, IObjectAddedEvent)
+def notify_user(presenter, event):
+    acl_users = getToolByName(presenter, 'acl_users')
+    mail_host = getToolByName(presenter, 'MailHost')
+    portal_url = getToolByName(presenter, 'portal_url')
+    
+    portal = portal_url.getPortalObject()
+    sender = portal.getProperty('email_from_address')
+    
+    if not sender:
+        return
+    
+    subject = "Is this you?"
+    message = "A presenter called %s was added here %s" % (presenter.title, presenter.absolute_url(),)
+    
+    matching_users = acl_users.searchUsers(fullname=presenter.title)
+    for user_info in matching_users:
+        email = user_info.get('email', None)
+        if email is not None:
+            mail_host.secureSend(message, email, sender, subject)
 
 class View(grok.View):
     grok.context(IPresenter)

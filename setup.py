65f5f6fe5abfabaf448b90711ebba95606e60400
Maurits van Rees <maurits@vanrees.org>
2011-05-25 15:48:44 +0000

    Only add the collective.autopermission dependency when using python2.4.
    svn path=/example.conference/trunk/; revision=239922

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
 from setuptools import setup, find_packages
 import os
+import sys
 
 version = '1.0a1'
+
+install_requires = [
+    'setuptools',
+    'Plone',
+    'plone.app.dexterity',
+    'plone.principalsource',
+    'plone.namedfile',
+    'plone.formwidget.namedfile',
+    'collective.wtf',
+    ]
+if sys.version_info < (2, 6):
+    # collective.autopermission is not needed on Zope 2.12, but hard
+    # to add as an extra for Plone 3, given our current buildout
+    # configs where an 'eggs += example.conference [plone3]' would
+    # conflict with the 'eggs -=' that is already there.  As long as
+    # we don't actually release to PyPI (giving different results when
+    # creating an sdist with python2.4 or python2.6) this setup should
+    # be fine too.
+    install_requires.append('collective.autopermission')
+
 
 setup(name='example.conference',
       version=version,
       description="Example accompanying http://plone.org/products/dexterity/documentation/manual/developers-manual/",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
@@ -23,21 +45,8 @@ setup(name='example.conference',
       namespace_packages=['example'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Plone',
-          'plone.app.dexterity',
-          'plone.principalsource',
-          'plone.namedfile',
-          'plone.formwidget.namedfile',
-          'collective.wtf',
-          # Next is not needed on Zope 2.12, but hard to add as an
-          # extra for Plone 3, given our current buildout configs
-          # where an 'eggs += example.conference [plone3]' would
-          # conflict with the 'eggs -=' that is already there.
-          'collective.autopermission',
-      ],
-      extras_require = {
+      install_requires=install_requires,
+      extras_require={
           'test':  ['collective.testcaselayer', 'plone.mocktestcase'],
           # Test relations within datagrid fields.  Some of these do
           # not yet have releases with the changes we need.
@@ -46,6 +55,7 @@ setup(name='example.conference',
               'plone.app.referenceablebehavior',
               'plone.formwidget.contenttree>1.0',
               ],
+          'plone3': ['collective.autopermission'],
           },
       entry_points="""
       [z3c.autoinclude.plugin]
